fb16bf99958e599c88f579377cd365fd87cfa2f5
zhiguangxu <xuzhiguang@gmail.com>
2016-10-19 17:42:19 +0000

    Just updated the component and route so as to be able to create new posts. Thanks to emberfire, the new post can not only be seen in the Chrome Ember Inspector but also in the Firebase dashboard.

--- app/components/create-new-post.js
+++ app/components/create-new-post.js
@@ -2,8 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     actions: {
-        createPost: function (newPost) {
-            console.log(newPost.title, newPost.author);
+        createPost: function (model) {
+            // console.log(model.title, model.author);
+            this.sendAction('createPost', model);
+
+            // Clear each input field
+            this.set('newPost.title', null);
+            this.set('newPost.author', null);
+            this.set('newPost.text', null);
         }
     }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model: function () {
         return {};
+    },
+    
+    actions: {
+        createPost: function (model) {
+          let post = this.store.createRecord('post', {
+            title: model.title,
+            text: model.text,
+            author: model.author,
+            createdDate: new Date()
+          });
+          post.save();
+        }
     }
 });
