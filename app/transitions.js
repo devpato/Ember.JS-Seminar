0427e442a8190935848e4b823deab4f6783b69ab
Patricio Vargas <fusion.style@hotmail.com>
2016-11-09 23:45:41 +0000

    Done with animated_publish_button

---
+++ app/transitions.js
@@ -0,0 +1,1 @@
+export default function(){        this.transition(            this.hasClass('publish-button'),            this.toValue(true),            this.use('toRight', { duration: 500 }),            this.reverse('toLeft', { duration: 500 })        );    }
